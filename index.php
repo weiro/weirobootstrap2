782bdc3b8511b161544b9053086edc699e58af1b
Anton Shevchik <biopsyh@gmail.com>
2014-01-06 16:39:15 +0400

    fix index file

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html>
   <head>
     <title>Bootstrap 101 Template</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset="UTF-8" name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="_/css/bootstrap.css" rel="stylesheet">
     <link href="_/css/mystyles.css" rel="stylesheet">
 
