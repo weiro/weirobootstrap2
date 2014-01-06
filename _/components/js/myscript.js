53cd1810ef43370aa3971d1bfa90c5410d8a6c64
Anton Shevchik <biopsyh@gmail.com>
2014-01-06 20:55:51 +0400

    fix

--- _/components/js/myscript.js
+++ _/components/js/myscript.js
@@ -1,8 +1,8 @@
 $(function  () {
-	//make menus drop auto
-	$('ul.nav li.dropdown').hover(function(){
-		$('.dropdown-menu', this).fadeIn();
-	}, function(){
-		$('.dropdown-menu', this).fadeOut('fast');
-	});//hover
-});//jQuery loaded
+  //make menus drop auto
+  $('ul.nav li.dropdown').hover(function(){
+    $('.dropdown-menu', this).fadeIn();
+  }, function(){
+    $('.dropdown-menu', this).fadeOut('fast');
+  }); //hover
+}); //jQuery loaded
