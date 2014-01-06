aaef29ded5a98094658f9796598cb2e6bb9da994
Anton Shevchik <biopsyh@gmail.com>
2014-01-06 20:08:36 +0400

    added navbar in header file

--- _/components/php/header.php
+++ _/components/php/header.php
@@ -4,6 +4,15 @@
             <section id="branding">
                 <a href="index.php"><img src="images/misc/ralogo_monogram.png" alt="logo"></a>
             </section> <!-- branding -->
+            <div class="navbar">
+            	<ul class="nav navbar-nav">
+            		<li><a href="index.php">Home</a></li>
+            		<li><a href="venuetravel.php">Venue/Travel</a></li>
+            		<li><a href="schedule.php">Schedule</a></li>
+            		<li><a href="artists.php">Artists</a></li>
+            		<li><a href="register.php">Register</a></li>
+            	</ul> <!-- navbar -->
+            </div>
         </header> <!-- header -->
     </div> <!-- column -->
 </div> <!-- content -->
