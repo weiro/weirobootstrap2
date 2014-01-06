b3e4b1f92b1d1e6af78603f10847eab0bc21ce79
Anton Shevchik <biopsyh@gmail.com>
2014-01-06 15:01:08 +0400

    Added basic index file

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>Welcome to my first app in Heroku</title>
+	<link rel="stylesheet" href="">
+</head>
+<body>
+	p{Welcome!}
+</body>
+</html>
